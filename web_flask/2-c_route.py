e86b3f1a0aa9a006cf7c5afcfd537c6480dcd759
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-31 18:16:44 -0500

    [add] script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_route():
+    """ Return message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def message_route():
+    """ Return message route /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def get_message_route(text):
+    """ Return message with text """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
