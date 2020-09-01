b22fbfce1250ad2aab90d45b44eb9cc67e871043
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-31 23:39:44 -0500

    [add] script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Return message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def message_route():
+    """ Return message route /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def get_message_route(text):
+    """ Return message with text """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """ Return message with text """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ return number only if n is an integer """
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
